bf14a94cad07753f2343a104777773fd6914ab83
remithzu <remithzu@hotmail.com>
2022-11-22 16:47:57 +0700

    fix multidex

--- app/src/main/java/com/rmtz/weather/app/WeatherApplication.kt
+++ app/src/main/java/com/rmtz/weather/app/WeatherApplication.kt
@@ -1,5 +1,13 @@
 package com.rmtz.weather.app
 
 import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
+import androidx.multidex.MultiDexApplication
 
-class WeatherApplication: Application() {}
+class WeatherApplication: MultiDexApplication() {
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+}
